e2653e8caa9671a958751051df2597262587cde1
Mario <jaimes.rene24@gmail.com>
2020-11-09 08:56:24 -0800

    Functional Database POG

--- GameDatabase/test/statements.sql
+++ GameDatabase/test/statements.sql
@@ -4,6 +4,11 @@ DROP TABLE customer;
 DROP TABLE storefront;
 DROP TABLE orders;
 DROP TABLE items;
+DROP TABLE sells;
+DROP TABLE prefers;
+DROP TABLE contains;
+--DROP TABLE supplies;
+--DROP TABLE makes;
 
 CREATE TABLE games(
 g_gameid decimal(10,0) not null,
@@ -14,11 +19,13 @@ g_rating char(4) not null,
 g_year decimal(4,0) not null
 );
 
+-- HAHA CHANGE THIS
+
 CREATE TABLE accessories(
 a_accid decimal(10,0) not null,
 a_name char(25) not null,
 a_type char(25) not null,
-a_official char(1) not null
+a_color char(25) not null
 );
 
 CREATE TABLE customer(
@@ -51,6 +58,27 @@ i_platform varchar(25) not null,
 i_type char(1) not null
 );
 
+CREATE TABLE sells(
+se_storeid decimal(10,0) not null,
+se_stockid decimal(10,0) not null,
+se_itemid decimal(10,0) not null,
+se_price decimal(6,2) not null,
+se_quantity decimal(5,0) not null,
+se_preowned char(1) not null
+);
+
+CREATE TABLE contains(
+co_orderid decimal(12,0) not null,
+co_storeid decimal(10,0) not null,
+co_stockid decimal(10,0) not null,
+co_quantity decimal(5,0) not null
+);
+
+CREATE TABLE prefers(
+p_custid decimal(10,0) not null,
+p_genre char(25) not null
+);
+
 .mode "csv"
 .separator "|"
 .import data/customer.csv customer
@@ -58,26 +86,11 @@ i_type char(1) not null
 .import data/games.csv games
 .import data/items.csv items
 .import data/orders.csv orders
+.import data/prefers.csv prefers
+.import data/sells.csv sells
+.import data/contains.csv contains
+.import data/accessories.csv accessories
 
-SELECT *
-FROM customer;
-
-SELECT '--------------------------------------------------';
-
-SELECT *
-FROM storefront;
-
-SELECT '--------------------------------------------------';
-
-SELECT *
-FROM games;
-
-SELECT '--------------------------------------------------';
-
-SELECT *
-FROM items;
-
-SELECT '--------------------------------------------------';
-
-SELECT *
-FROM orders;
+SELECT i_name
+FROM customer, orders, contains, items, sells
+WHERE c_custid = o_custid AND o_orderid = co_orderid AND co_stockid = se_stockid AND co_storeid = se_storeid AND se_itemid = i_itemid AND c_name = "Uma Shaw";

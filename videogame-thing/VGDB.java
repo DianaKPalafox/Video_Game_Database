1b3f7d2052939a7c9830bdbc2d473f9c72e4fe6d
Mario <jaimes.rene24@gmail.com>
2020-12-05 15:12:42 -0800

    Added imports, database reset

--- videogame-thing/VGDB.java
+++ videogame-thing/VGDB.java
@@ -71,10 +71,7 @@ public class VGDB {
         try{
             Statement s = c.createStatement();
             s.setQueryTimeout(30);
-            String sql = "drop table customer drop table storefront drop table games drop table items";
-            sql = ".mode \"csv\" .separator \"|\" .import data/customer.csv customer .import data/storefront.csv storefront .import data/games.csv games .import data/items.csv items .import data/orders.csv orders .import data/prefers.csv prefers .import data/sells.csv sells .import data/contains.csv contains .import data/accessories.csv accessories";
             s.executeUpdate("DROP TABLE IF EXISTS warehouse");
-            s.executeUpdate(sql);
         }
         catch(SQLException e){
             System.err.println(e.getMessage());
@@ -119,10 +116,120 @@ public class VGDB {
         System.out.println("++++++++++++++++++++++++++++++++++");
     }
     
+    private void resetDatabase()
+    {
+        try{
+            Statement s = c.createStatement();
+            s.setQueryTimeout(30);
+            
+            s.executeUpdate("DROP TABLE IF EXISTS customer");
+            s.executeUpdate("DROP TABLE IF EXISTS storefront");
+            s.executeUpdate("DROP TABLE IF EXISTS games");
+            s.executeUpdate("DROP TABLE IF EXISTS items");
+            s.executeUpdate("DROP TABLE IF EXISTS orders");
+            s.executeUpdate("DROP TABLE IF EXISTS prefers");
+            s.executeUpdate("DROP TABLE IF EXISTS sells");
+            s.executeUpdate("DROP TABLE IF EXISTS contains");
+            s.executeUpdate("DROP TABLE IF EXISTS accessories");
+            
+            s.executeUpdate("CREATE TABLE games(g_gameid decimal(10,0) not null, g_name char(25) not null, g_solo char(1) not null, g_genre char(25) not null, g_rating char(4) not null, g_year decimal(4,0) not null)");
+            s.executeUpdate("CREATE TABLE accessories(a_accid decimal(10,0) not null, a_name char(25) not null, a_type char(25) not null, a_color char(25) not null)");
+            s.executeUpdate("CREATE TABLE customer(c_custid decimal(10,0) not null, c_name varchar(40) not null, c_addy varchar(100) not null, c_phone char(15) not null);");
+            s.executeUpdate("CREATE TABLE storefront(s_storeid decimal(10,0) not null, s_name char(25) not null, s_addy varchar(50) not null, s_phone char(14) not null);");
+            s.executeUpdate("CREATE TABLE orders(o_orderid decimal(12,0) not null, o_custid decimal(10,0) not null, o_storeid decimal(10,0) not null, o_orderdate date not null, o_orderstatus char(1) not null);");
+            s.executeUpdate("CREATE TABLE items(i_itemid decimal(10,0) not null, i_genericid decimal(10,0) not null, i_name varchar(25) not null, i_platform varchar(25) not null, i_type char(1) not null);");
+            s.executeUpdate("CREATE TABLE sells(se_storeid decimal(10,0) not null, se_stockid decimal(10,0) not null, se_itemid decimal(10,0) not null, se_quantity decimal(5,0) not null, se_preowned char(1) not null);");
+            s.executeUpdate("CREATE TABLE contains(co_orderid decimal(12,0) not null, co_storeid decimal(10,0) not null, co_stockid decimal(10,0) not null, co_quantity decimal(5,0) not null);");
+            s.executeUpdate("CREATE TABLE prefers(p_custid decimal(10,0) not null, p_genre char(25) not null);");
+            
+            readCSV();
+            
+            System.out.println("Complete");
+        }
+        catch(SQLException e){
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    private void readCSV()
+    {
+        importCustomer();
+        importGames();
+        //add other imports
+    }
+    
+    private void importCustomer()
+    {
+        try{
+            Statement s = c.createStatement();
+            s.setQueryTimeout(30);
+            
+            BufferedReader line = new BufferedReader(new FileReader("data/customer.csv"));
+            String linetext = null;
+            
+            String sql = "INSERT INTO customer(c_custid, c_name, c_addy, c_phone) VALUES(?,?,?,?)";
+            PreparedStatement ps = c.prepareStatement(sql);
+            while ((linetext = line.readLine()) != null)
+            {
+                String[] data = linetext.split("|");
+                int id = Integer.parseInt(data[0]);
+                String name = data[1];
+                String addy = data[2];
+                String phone = data[3];
+                
+                ps.setInt(1, id);
+                ps.setString(2, name);
+                ps.setString(3, addy);
+                ps.setString(4, phone);
+                ps.executeUpdate();
+            }
+            ps.close();
+            
+        }
+        catch(Exception e){
+            System.err.println("Customer oops");
+        }
+    }
+    
+    private void importGames()
+    {
+        try{
+            Statement s = c.createStatement();
+            s.setQueryTimeout(30);
+            
+            BufferedReader line = new BufferedReader(new FileReader("data/games.csv"));
+            String linetext = null;
+            
+            String sql = "INSERT INTO games(g_gameid, g_name, g_solo, g_genre, g_rating, g_year) VALUES(?,?,?,?,?,?)";
+            PreparedStatement ps = c.prepareStatement(sql);
+            while ((linetext = line.readLine()) != null)
+            {
+                String[] data = linetext.split("|");
+                int id = Integer.parseInt(data[0]);
+                String name = data[1];
+                String solo = data[2];
+                String genre = data[3];
+                String rating = data[4];
+                int year = Integer.parseInt(data[5]);
+                
+                ps.setInt(1, id);
+                ps.setString(2, name);
+                ps.setString(3, solo);
+                ps.setString(4, genre);
+                ps.setString(5, rating);
+                ps.setInt(6, year);
+                ps.executeUpdate();
+            }
+            ps.close();
+        }
+        catch(Exception e){
+            System.err.println("Games oops");
+        }
+    }
+    
     private void commenceSearch()
     {
         try {
-            
             Statement s = c.createStatement();
             s.setQueryTimeout(30);
             String sql1 = "SELECT * FROM items WHERE i_name LIKE '";
@@ -150,11 +257,8 @@ public class VGDB {
                 int sid = rs.getInt("w_suppkey");
                 int nid = rs.getInt("w_nationkey");
                 
-                printer.printf("%10s %-40s %10s %10s %10s\n", "" + id, name, "" + cap, "" + sid, "" + nid);
+                //printer.printf("%10s %-40s %10s %10s %10s\n", "" + id, name, "" + cap, "" + sid, "" + nid);
             }
-            
-            printer.close();
-            writer.close();
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
@@ -169,7 +273,7 @@ public class VGDB {
         sj.openConnection("data/games.sqlite");
         //it runs but it doesn't do anything lol
         
-        
+        sj.resetDatabase();
         
         //sj.createTable();
         //sj.Q1();
